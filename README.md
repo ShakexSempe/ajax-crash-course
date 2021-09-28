2875c643360aef0bb5ba434247f8d3bd82771573
Ray-Shakex <bshakex@gmail.com>
2021-09-28 10:50:21 +0200

    README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ajax-crash-course
 vanilla javascript AJAX fundementals: presented by Brad Traversy
+
+## HTTP STATUSES
+    // 200: "OK"
+    // 403: "forbidden"
+    // 404: "not found"
+
+## readyState VALUES
+    // 0: request not initialized
+    // 1: server connection established
+    // 2: request received
+    // 3: processing request
+    // 4: request finished and response is ready
+
--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-// HTTP statuses
-    // 200: "OK"
-    // 403: "forbidden"
-    // 404: "not found"
-    // readyState values
-    // 0: request not initialized
-    // 1: server connection established
-    // 2: request received
-    // 3: processing request
-    // 4: request finished and response is ready
-        
+
 // create event listener
 document.getElementById('button')
     .addEventListener("click", loadText);
@@ -35,7 +25,3 @@ function loadText() {
     xhr.send();
 }
 // end of load text ()
-        
-        
-        
-        
