ddd7a896b8563eb8cb6f4b6a74e0fc23d6b2b3c8
Ray-Shakex <bshakex@gmail.com>
2021-09-30 09:37:33 +0200

    .onload()

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // create event listener
 document.getElementById('button')
     .addEventListener("click", loadText);
@@ -7,19 +6,23 @@ function loadText() {
     // create XHR object
     const xhr = new XMLHttpRequest();
     // open() 3 params: -type, url/filename, async
-
     xhr.open('GET', 'sample.txt', true);
-    console.log('READYSTATE: ', xhr.readyState)
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // console.log(this.responseText);
+    console.log('READYSTATE: ', xhr.readyState);
+    xhr.onload = function() {
+        console.log('READYSTATE: ', xhr.readyState);
+        if(this.status == 200) {
         }
     }
 
-    // xhr.onload = function() {
-    //     if(this.status == 200) {
+    // on-ready-state-change method
+    // xhr.onreadystatechange = function () {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         console.log(this.responseText);
     //     }
     // }
+    // end of on-ready-state-change method
+
+
 
     // sends request
     xhr.send();
