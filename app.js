f7de3b7e767cc7124cd9db36233f63ab16806fe6
Ray-Shakex <bshakex@gmail.com>
2021-09-30 11:13:24 +0200

    .onload()

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ loadText = () => {
     // create XHR object
     const xhr = new XMLHttpRequest();
     // open() 3 params: -type, url/filename, async
-    xhr.open('GET', 'sample.txt', true);
+    xhr.open('GET', 'sample2.txt', true);
     console.log('READYSTATE: ', xhr.readyState);
 
     // OPTIONAL - used for loaders
@@ -14,8 +14,16 @@ loadText = () => {
 
     xhr.onload = () => {
         console.log('READYSTATE: ', xhr.readyState);
-        if(this.status == 200) {
+        if(xhr.status == 200) {
+            document.getElementById("text").innerHTML = xhr.responseText;
+        } else if (xhr.status == 404) {
+            document.body.style.backgroundColor = 'red';
+            document.getElementById("text").innerHTML = 'Not Found'
         }
+    }
+
+    xhr.onerror = () => {
+        console.log('request error..');
     }
 
     // on-ready-state-change method
