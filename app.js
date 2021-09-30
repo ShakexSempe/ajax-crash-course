2a14aa73447a7d32edd70e824e5abbfd7e85b487
Ray-Shakex <bshakex@gmail.com>
2021-09-30 09:42:13 +0200

    functions to arrow functions

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// create event listener
-document.getElementById('button')
-    .addEventListener("click", loadText);
+
 // load text ()
-function loadText() {
+loadText = () => {
     // create XHR object
     const xhr = new XMLHttpRequest();
     // open() 3 params: -type, url/filename, async
     xhr.open('GET', 'sample.txt', true);
     console.log('READYSTATE: ', xhr.readyState);
-    xhr.onload = function() {
+
+    // OPTIONAL - used for loaders
+    xhr.onprogress = () => {
+        console.log('READYSTATE', xhr.readyState)
+    }
+
+    xhr.onload = () => {
         console.log('READYSTATE: ', xhr.readyState);
         if(this.status == 200) {
         }
@@ -22,9 +26,12 @@ function loadText() {
     // }
     // end of on-ready-state-change method
 
-
-
     // sends request
     xhr.send();
 }
 // end of load text ()
+
+// create event listener
+document.getElementById('button')
+    .addEventListener("click", loadText);
+// end of event listener
