685877a1371c7aecb1553da6915e5e387fbb8876
Ray-Shakex <bshakex@gmail.com>
2021-10-05 09:23:10 +0200

    ajax2 and json files

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-
 // load text ()
 loadText = () => {
     // create XHR object
     const xhr = new XMLHttpRequest();
     // open() 3 params: -type, url/filename, async
-    xhr.open('GET', 'sample2.txt', true);
+    xhr.open('GET', 'sample.txt', true);
     console.log('READYSTATE: ', xhr.readyState);
 
     // OPTIONAL - used for loaders
     xhr.onprogress = () => {
-        console.log('READYSTATE', xhr.readyState)
+        console.log('READYSTATE: ', xhr.readyState);
     }
 
     xhr.onload = () => {
         console.log('READYSTATE: ', xhr.readyState);
-        if(xhr.status == 200) {
+        if (xhr.status == 200) {
             document.getElementById("text").innerHTML = xhr.responseText;
         } else if (xhr.status == 404) {
             document.body.style.backgroundColor = 'red';
-            document.getElementById("text").innerHTML = 'Not Found'
+            document.getElementById("text").innerHTML = 'Not Found';
         }
     }
 
