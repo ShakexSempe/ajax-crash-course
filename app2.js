181f017745c534b9a4bd0ab99a6443b59ce14b91
Ray-Shakex <bshakex@gmail.com>
2021-10-07 10:36:49 +0200

    fetch local json: loadUsers()

--- app2.js
+++ app2.js
@@ -9,16 +9,16 @@ loadUser = () => {
     xhr.open('GET', 'user.json', true);
 
     xhr.onload = () => {
-        if(xhr.status === 200) {
+        if (xhr.status === 200) {
             const user = JSON.parse(xhr.responseText);
             console.log(user.name);
 
             // ES5 method
             let output = '';
             output += '<ul>' +
-                '<li>ID: '+user.id+'</li>' +
-                '<li>Name: '+user.id+'</li>' +
-                '<li>Email: '+user.email+'</li>' +
+                '<li>ID: ' + user.id + '</li>' +
+                '<li>Name: ' + user.name + '</li>' +
+                '<li>Email: ' + user.email + '</li>' +
                 '</ul>';
             document.getElementById('user').innerHTML = output;
             // end of ES5 method
@@ -28,5 +28,28 @@ loadUser = () => {
     xhr.send();
 }
 
+loadUsers = () => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'users.json', true);
+
+    xhr.onload = () => {
+        if(xhr.status == 200) {
+            let users = JSON.parse(xhr.responseText);
+            let output = '';
+            console.log(users);
+            for(let i in users){
+                output += '<ul>' +
+                    '<li>ID: '+users[i].id+'</li>' +
+                    '<li>Name: '+users[i].name+'</li>' +
+                    '<li>Email: '+users[i].email+'</li>' +
+                    '</ul>';
+            }
+            document.getElementById("users").innerHTML = output;
+        }
+    }
+
+    xhr.send();
+}
 // BUTTON EVENT LISTENERS
 btn1.addEventListener("click", loadUser);
+btn2.addEventListener("click", loadUsers)
